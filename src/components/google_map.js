74a059f83ced4f51d60c64c83daf547cb9b0d71a
yehong you <youyehong@gmail.com>
2017-05-10 15:51:45 +1000

    finish the demo

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-
+// the video intro the import lib googlemap, not flowing, in 68
 // ref system in react, anywhere call this.refs.ref-value, will give you actual html element
 class GoogleMap extends Component {
   // life cycle mehod, get call after the components render in the screen
-  componentDidMount(){  // this arg,googlemap will find the this.refs.map html element on the screen and embed to it 
+  componentDidMount(){  // this arg,googlemap will find the this.refs.map html element on the screen and embed to it
     new google.maps.Map(this.refs.map,{  // creat google map inside our doc
       zoom: 12,
       center:{
